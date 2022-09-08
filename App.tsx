e89652aeacc47c74245934c4b73747485ac70b1f
Yukiya Nakagawa <yukiya@onemile.co.jp>
2022-09-08 12:46:27 +0900

    feat: Webでバーコードリーダーを読めるようにしてみました

--- App.tsx
+++ App.tsx
@@ -1,10 +1,85 @@
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Camera, CameraType } from 'expo-camera';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [scanned, setScanned] = useState(false);
+
+  if (!permission) {
+    return (
+      <View style={styles.container}>
+        <Text>Requesting permission</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    console.log(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  function toggleCameraType() {
+    setType((current) => (
+      current === CameraType.back ? CameraType.front : CameraType.back
+    ));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Camera
+        style={styles.camera}
+        type={type}
+        barCodeScannerSettings={{
+          barCodeTypes: [
+            BarCodeScanner.Constants.BarCodeType.aztec,
+            BarCodeScanner.Constants.BarCodeType.codabar,
+            BarCodeScanner.Constants.BarCodeType.code39,
+            BarCodeScanner.Constants.BarCodeType.code93,
+            BarCodeScanner.Constants.BarCodeType.code128,
+            BarCodeScanner.Constants.BarCodeType.code39mod43,
+            BarCodeScanner.Constants.BarCodeType.datamatrix,
+            BarCodeScanner.Constants.BarCodeType.ean13,
+            BarCodeScanner.Constants.BarCodeType.ean8,
+            BarCodeScanner.Constants.BarCodeType.interleaved2of5,
+            BarCodeScanner.Constants.BarCodeType.itf14,
+            BarCodeScanner.Constants.BarCodeType.maxicode,
+            BarCodeScanner.Constants.BarCodeType.pdf417,
+            BarCodeScanner.Constants.BarCodeType.rss14,
+            BarCodeScanner.Constants.BarCodeType.rssexpanded,
+            BarCodeScanner.Constants.BarCodeType.upc_a,
+            BarCodeScanner.Constants.BarCodeType.upc_e,
+            BarCodeScanner.Constants.BarCodeType.upc_ean,
+            BarCodeScanner.Constants.BarCodeType.qr,
+          ],
+        }}
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+      >
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={toggleCameraType}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +88,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
